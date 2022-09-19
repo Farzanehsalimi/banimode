6588717bdc3f3a97fc9feafd141a3cab058ca9d3
Farzaneh Salimi <farzanehsalimi16@gmail.com>
2022-09-19 20:51:14 +0430

    carousel

---
+++ main.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
